bec55fd2565db2ccda7b7e68392cbcdea269da49
mochizuki875 <mzk875@gmail.com>
2022-03-30 16:29:05 +0900

    http channel

---
+++ http/listenandserve-channel.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	// "fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+var layout = "2006-01-02 15:04:05"
+func timeToString(t time.Time) string {
+    str := t.Format(layout)
+    return str
+}
+
+func main() {
+	// channel初期化
+	q := make(chan string)
+	
+	//Handler定義
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		log.Println("[Handler] Call helloHandler")
+		
+		q <- timeToString(time.Now()) // channel送信
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	// channel受信用goroutine起動
+	go func(){
+		log.Println("[goroutine] Start goroutine & Receive Chennel")
+		for {
+			time.Sleep(3*time.Second)
+			msg := <-q
+			log.Println("[goroutine]Receive Channel:", msg)
+		}
+	}()
+
+	log.Println("[main] http.ListenAndServe(\":8080\", nil)")
+
+	// Handlerとパスの対応付け
+	http.HandleFunc("/", helloHandler)
+
+	// httpサーバー起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+
+}
--- http/listenandserve-queue.go
+++ http/listenandserve-queue.go
@@ -4,55 +4,41 @@ import (
 	"io"
 	"log"
 	"net/http"
-	// "time"
+	"time"
 )
 
-// チャンネルを構造体のメンバーとして定義
-type Queue struct {
-	reqCh chan string
+var layout = "2006-01-02 15:04:05"
+func timeToString(t time.Time) string {
+    str := t.Format(layout)
+    return str
 }
-
-// メソッドを定義
-func (q * Queue) EnQueue(w http.ResponseWriter, r *http.Request) {
-	log.Println("[Debug] Before sent")
-	q.reqCh <- r.URL.String()
-	io.WriteString(w, "Hello world")
-
-	now := <- q.reqCh
-	log.Println(now)
-}
-
 
 func main() {
 	// Hello world, the web server
 
-	q := Queue{reqCh: make(chan string)}
+	// queueになるチャンネルを定義
+	reqCh := make(chan string) 
 
 	// HTTP Handler定義
-	// helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 
-	// 	log.Println("[Debug] Before sent")
+		log.Println("[Debug] Before sent")
 
-	// 	// reqCh <- timeToString(time.Now())
-	// 	reqCh <- 1
+		reqCh <- timeToString(time.Now()) // ここで止まる？
 
-	// 	log.Println("[Debug] After sent")
+		log.Println("[Debug] After sent")
 
-	// 	log.Println("Call helloHandler")
-	// 	io.WriteString(w, "Hello, world!\n")
-	// 	// io.WriteString(w, "Hello, world!\n")
+		log.Println("Call helloHandler")
+		io.WriteString(w, "Hello, world!\n")
+		// io.WriteString(w, "Hello, world!\n")
 
-	// 	now := <- reqCh
-	// 	log.Println(now)
+		now := <- reqCh
+		log.Println(now)
 
-	// }
+	}
 
 	// Handlerとパスの対応付け
-	// http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/", q.EnQueue)
-
-
-
+	http.HandleFunc("/", helloHandler)
 
 	log.Println("[Debug] http.ListenAndServe(\":8080\", nil)")
 	// httpサーバー起動
